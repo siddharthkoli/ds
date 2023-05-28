4c1956ef616ad6579f6f457406d13407ca35b585
Siddharth Koli <55916425+siddharthkoli@users.noreply.github.com>
2023-05-28 16:06:00 +0530

    Update bully.java

--- final/bully.java
+++ final/bully.java
@@ -1,75 +1,63 @@
 import java.util.*;
 
+class Process {
+    int id;
+    boolean active;
 
-class Process{
-    int processId;
-    boolean isActive;
-    public Process(int id){
-        this.processId=id;
-        this.isActive=true;
+    Process(int id) {
+        this.id = id;
+        this.activate = false;
     }
+    
+    public void election(List<Process> processes) {
+        int highestId = 0;
+        for (Process process: processes)
+            highestId = Math.max(highestId, process.id);
 
-    public void election(List<Process> processes){
-        int highestId=0;
-        for(Process process: processes){
-            if(process.processId>highestId){
-                highestId=process.processId;
-            }
-        }
-        if(this.processId==highestId){
+        if (highestId == this.id) {
             announceCoordinator();
-        }
-        else{
-            for(Process process: processes){
-                if(process.processId>processId){
-                    sendelectionmessage(process);
-                }
-            }
+        } else {
+            for (Process process: processes)
+                if (this.id < process.id)
+                    this.sendElectionMessageToProcess(process);
         }
     }
 
-    public void announceCoordinator(){
-        System.out.println("Process "+processId+" is selected as the coordinator");
+    public void announceCoordinator() {
+        sysout process id is selected as coordinator;
     }
 
-    public void sendelectionmessage(Process process){
-        System.out.println("Election message received by "+process.processId +" from "+processId);
+    public void sendElectionMessageToProcess(Process process) {
+        sysout Eelection msg received by process.id from this.id;
 
-        if(process.isActive){
-            process.sendresponse(processId);
-        }
+        if (process.active)
+            this.sendResponse(process.processId);
     }
-    public void sendresponse(int senderId){
-        System.out.println("Process "+ senderId+" received Response from "+ processId);
+
+    public void sendResponse(int receiverId) {
+        sysout Process receiverId received msg from this.id;
     }
 }
-public class bully2{
-    public static void main(String args[]){
+
+public class Bully {
+    public static void main(String[] args) {
         Scanner sc= new Scanner(System.in);
         System.out.println("Enter The number of Processes : ");
         int num=sc.nextInt();
 
         List<Process> processes= new ArrayList<>();
         for (int i=1;i<=num;i++){
-            Process process=new Process(i);
-            processes.add(process);
+            processes.add(new Process(i));
         }
         System.out.println("Enter The process to be made inactive : ");
         num=sc.nextInt();
 
-        for(Process process : processes){
-            if(process.processId==num){
-                process.isActive=false;
-            }
-        }
+        processes.get(i-1).active = false;
 
         for(Process process : processes){
             if(process.isActive){
                 process.election(processes);
             }
         }
-
-
-
     }
 }
